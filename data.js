1a4bc3b3e95e16f937fdb254ef22260bc0c49ec4
Athanas Mochama <athanasmochama253@gmail.com>
2025-12-19 23:46:40 +0300

    Fix script order with defer and load data before index

---
+++ data.js
@@ -0,0 +1,21 @@
+const bookStore = {
+    name: "Flatbooks Technical Books",
+    books: [
+      {
+        title: "Eloquent JavaScript: A Modern Introduction to Programming",
+        author: "Marijn Haverbeke",
+        image: "https://images-na.ssl-images-amazon.com/images/I/41as+WafrFL._SX376_BO1,204,203,200_.jpg"
+      },
+      {
+        title: "HTML and CSS: Design and Build Websites",
+        author: "Jon Duckett",
+        image: "https://images-na.ssl-images-amazon.com/images/I/51b5YG6Y1rL._SX258_BO1,204,203,200_.jpg"
+      },
+      {
+        title: "JavaScript and JQuery: Interactive Front-End Web Development",
+        author: "Jon Duckett",
+        image: "https://images-na.ssl-images-amazon.com/images/I/51qlgJ6+hPL._SX258_BO1,204,203,200_.jpg"
+      }
+    ]
+  };
+  
